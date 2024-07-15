64cdb0f3506efbd7ccce61669e0f49b58c70e7ae
linearesdiego <diegolineares@gmail.com>
2024-07-15 12:21:17 -0300

    productos y proveedores

--- src/routes/productos.router.js
+++ src/routes/productos.router.js
@@ -9,34 +9,72 @@ const {
   deleteProduct,
 } = require("../services/productos.services");
 
-const { createProductoSchema,
+const {
+  createProductoSchema,
   updateProductoSchema,
   getProductoSchema,
-  queryProductoSchema } = require('../schemas/producto.schema');
+  queryProductoSchema,
+} = require("../schemas/producto.schema");
 
 //obtener prodcutos
-router.get("/",
-  validatorHandler(queryProductoSchema, 'query'),
+router.get(
+  "/",
+  validatorHandler(queryProductoSchema, "query"),
   async (req, res) => {
     try {
       const { negocioId } = req.query;
       const productos = await findAll(negocioId);
+      //se debe poder filtra por sucursal y categoria
       res.status(200).json({
         ok: true,
-        data: productos,
+        data: [
+          {
+            nombre: "coca cola",
+            precio: 10,
+            descipcion: "aaaa",
+            sucursalId: 2,
+            categoria: {
+              idCategoria: 8,
+              nombre: "bebidas",
+            },
+            imagen: null,
+          },
+          {
+            nombre: "pepsi",
+            precio: 15,
+            descipcion: "aaaa",
+            sucursalId: 1,
+            categoria: {
+              idCategoria: 8,
+              nombre: "bebidas",
+            },
+            imagen: null,
+          },
 
+          {
+            nombre: "papas",
+            precio: 5,
+            descipcion: "aaaa",
+            sucursalId: 2,
+            categoria: {
+              idCategoria: 9,
+              nombre: "snacks",
+            },
+            imagen: null,
+          },
+        ],
       });
     } catch (error) {
       res.status(500).json({
         message: "Error al obtener los productos",
         error: error.message,
-      })
-
-
+      });
     }
-  });
+  }
+);
 //guardar productos
-router.post("/",
+router.post(
+  "/",
   validatorHandler(createProductoSchema, "body"),
   async (req, res) => {
     try {
@@ -46,17 +84,19 @@ router.post("/",
       res.status(200).json({
         message: "product created",
         data: {
-          creted: "creted"
-        }
+          creted: "creted",
+        },
       });
     } catch (err) {
       next(err);
     }
-  });
+  }
+);
 //actualizar productos
-router.patch("/:productoId",
-  validatorHandler(getProductoSchema, 'params'),
-  validatorHandler(updateProductoSchema, 'body'),
+router.patch(
+  "/:productoId",
+  validatorHandler(getProductoSchema, "params"),
+  validatorHandler(updateProductoSchema, "body"),
   async (req, res) => {
     try {
       const { productoId } = req.params;
@@ -68,10 +108,12 @@ router.patch("/:productoId",
     } catch (err) {
       next(err);
     }
-  });
+  }
+);
 //eliminar productos
-router.delete("/:productoId",
-  validatorHandler(getProductoSchema, 'params'),
+router.delete(
+  "/:productoId",
+  validatorHandler(getProductoSchema, "params"),
   async (req, res) => {
     try {
       const { productoId } = req.params;
@@ -82,5 +124,6 @@ router.delete("/:productoId",
     } catch (err) {
       next(err);
     }
-  });
+  }
+);
 module.exports = router;
