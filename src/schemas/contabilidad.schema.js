c33e4654241bee20a5614c2c337e94fc33ec37fd
DanielCastillaa <danielcastillaa123@gmail.com>
2024-07-25 16:12:32 -0300

    create the accounting schema and router

---
+++ src/schemas/contabilidad.schema.js
@@ -0,0 +1,22 @@
+const joi = require('joi');
+
+const id = joi.number().integer();
+
+
+const createCompraSchema = joi.object({
+});
+
+const createGastoSchema = joi.object({
+});
+
+const createVentaSchema = joi.object({
+});
+
+const getCompraSchema = joi.object({
+});
+
+const getGastoSchema = joi.object({
+});
+
+const getVentaSchema = joi.object({
+});
